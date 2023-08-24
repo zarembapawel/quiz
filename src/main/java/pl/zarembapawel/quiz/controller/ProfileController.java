4e9d09536afad86db96b4156cff0f068791f3713
zarembapawel <zarembapawel95@gmail.com>
2023-08-24 22:05:02 +0200

    Get profile & updateProfilePoints

--- src/main/java/pl/zarembapawel/quiz/controller/ProfileController.java
+++ src/main/java/pl/zarembapawel/quiz/controller/ProfileController.java
@@ -3,10 +3,8 @@ package pl.zarembapawel.quiz.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pl.zarembapawel.quiz.model.Profile;
 import pl.zarembapawel.quiz.service.ProfileService;
 
@@ -17,9 +15,25 @@ public class ProfileController {
 
     private ProfileService service;
 
+    @GetMapping("/{name}")
+    public Profile getByName(@PathVariable String name) {
+        Profile profile = service.get(name);
+        if (profile == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return profile;
+    }
+
     @PostMapping
     public ResponseEntity<Void> add(@RequestBody Profile profile) {
         HttpStatus status = service.add(profile);
+        return new ResponseEntity<>(status);
+    }
+
+    @PutMapping("/updateProfilePoints/{name}/{points}")
+    public ResponseEntity<Void> updateProfilePoints(@PathVariable String name,
+                                                    @PathVariable Integer points) {
+        HttpStatus status = service.updateProfilePoints(name, points);
         return new ResponseEntity<>(status);
     }
 }
--- src/main/java/pl/zarembapawel/quiz/service/ProfileService.java
+++ src/main/java/pl/zarembapawel/quiz/service/ProfileService.java
@@ -37,4 +37,18 @@ public class ProfileService {
         return entity.map(profileEntity -> mapper.map(profileEntity, Profile.class))
                      .orElse(null);
     }
+
+    public HttpStatus updateProfilePoints(String name, Integer points) {
+        log.info("Updating profile {} points {}", name, points);
+        Profile profile = get(name);
+        if (profile == null) {
+            log.error("Profile don't exist");
+            return HttpStatus.NOT_FOUND;
+        }
+        log.info("Points before update {}", profile.getPoints());
+        profile.setPoints(profile.getPoints() + points);
+        log.info("Points after update {}", profile.getPoints());
+        repository.save(mapper.map(profile, ProfileEntity.class));
+        return HttpStatus.NO_CONTENT;
+    }
 }
